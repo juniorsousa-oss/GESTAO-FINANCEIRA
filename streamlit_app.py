c00c40686c15dfdb01d3b720fe70bc444e9c217c
juniorsousa-oss <juniorsousa@unipam.edu.br>
2026-09-18 22:37:22 -0300

    Remove topo e consolida layout com sidebar fixa

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from modules import configuracoes, dashboard, dividas, importacao, movimentacoes, previsoes, saldos
 from services.db import is_configured
-from services.ui import inject_global_css, render_sidebar, render_topbar
+from services.ui import inject_global_css, render_page_header, render_sidebar
 
 
 st.set_page_config(
@@ -30,7 +30,7 @@ PAGE_META = {
 inject_global_css(st.session_state["view_mode"])
 page = render_sidebar(is_configured())
 page_title, subtitle = PAGE_META[page]
-view_mode = render_topbar(page_title, subtitle)
+view_mode = render_page_header(page_title, subtitle)
 inject_global_css(view_mode)
 
 if page == "Dashboard":
