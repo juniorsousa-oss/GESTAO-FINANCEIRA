69c71ea1e14253984cd9f3831261f8085840d4d9
juniorsousa-oss <juniorsousa@unipam.edu.br>
2026-09-19 13:31:47 -0300

    Conecta controle de sidebar fluido sem reexecutar dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from modules import configuracoes, dashboard, dividas, importacao, movimentacoes, previsoes, saldos
 from services.db import is_configured
-from services.ui import inject_global_css, render_app_header, render_page_header, render_sidebar, render_sidebar_open_button
+from services.ui import inject_global_css, render_app_header, render_page_header, render_sidebar, render_sidebar_toggle
 
 
 st.set_page_config(
@@ -31,7 +31,7 @@ PAGE_META = {
 
 inject_global_css(st.session_state["view_mode"])
 render_app_header()
-render_sidebar_open_button()
+render_sidebar_toggle()
 page = render_sidebar(is_configured())
 view_mode = st.session_state.get("view_mode", "Desktop")
 
